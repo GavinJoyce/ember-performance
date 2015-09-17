5e10e960d2bbdc0becff3e72dc2ef65659145175
Gavin Joyce <gavinjoyce@gmail.com>
2015-09-17 08:42:23 +0100

    temp: add canary

--- app/app.js
+++ app/app.js
@@ -74,7 +74,10 @@
       compilerPath: '/ember/ember-2.0.2.template-compiler.js' },
     { name: '2.1.0-beta.4',
       path: '/ember/ember-2.1.0-beta.4.prod.js',
-      compilerPath: '/ember/ember-2.1.0-beta.4.template-compiler.js' }
+      compilerPath: '/ember/ember-2.1.0-beta.4.template-compiler.js' },
+    { name: 'canary',
+      path: '/ember/ember-canary.prod.js',
+      compilerPath: '/ember/ember-canary.template-compiler.js' }
   ];
 
   // This should probably be ember-cli, it just seemed so complicated to
